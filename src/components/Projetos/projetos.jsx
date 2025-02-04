dee7dc18db6caec9eb21c034ea2691e65b8992a3
Pedro Leonardo <leo.dev.ny@gmail.com>
2025-02-04 16:24:29 -0300

    animation no footer

--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -1,10 +1,5 @@
 import './projetos.css'
 function Projetos(){
-  let curve = document.querySelector('#curve')
-  window.addEventListener('scroll', function(){
-    let value = 1+window.scrollY/-600
-    curve.style.transform = `scaleY(${value})`
-  })
   return(
     <>
       <section id='projetos'>
