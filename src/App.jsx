65a008edd7921edc7d8f92871e0c5a6e27c299d2
Pedro Leonardo <leo.dev.ny@gmail.com>
2025-02-04 14:50:00 -0300

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Header from './components/Header/header'
 import Main from './components/Main/main'
 import Footer from './components/Footer/footer'
+import Projetos from './components/Projetos/projetos'
 function App() {
   return (
     <>
       <Header/>
       <Main/>
+      <Projetos/>
       <Footer/>
     </>
   )
--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -1,9 +1,17 @@
 import './projetos.css'
 function Projetos(){
+  let curve = document.querySelector('#curve')
+  window.addEventListener('scroll', function(){
+    let value = 1+window.scrollY/-600
+    curve.style.transform = `scaleY(${value})`
+  })
   return(
     <>
-    <section id='projetos'>
-    </section>
+      <section id='projetos'>
+        <section className='curve'>
+          <span id='curve'></span>
+        </section>
+      </section>
     </>
   )
 }
